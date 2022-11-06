3cc49043a9e1f2d9865421e495ae05f465b8a0d1
James Ball <jamie.j.ball@gmail.com>
2022-11-06 09:05:32 -0800

    feat: add before you go page

--- pages/before.tsx
+++ pages/before.tsx
@@ -1,5 +1,240 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
 const Before = () => {
-  return <div>Before</div>;
+  return (
+    <div>
+      <Navbar />
+      <section className="text-white ">
+        <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+          <div className="max-w-lg mx-auto text-center">
+            <h2 className="text-3xl font-bold text-transparent sm:text-4xl bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text">
+              Before you go
+            </h2>
+
+            <p className="mt-4 text-gray-300">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Consequuntur aliquam doloribus nesciunt eos fugiat. Vitae aperiam
+              fugit consequuntur saepe laborum.
+            </p>
+          </div>
+
+          <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3">
+            <a
+              className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:border-teal-500/10 hover:shadow-teal-500/10"
+              href="/services/digital-campaigns"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-10 h-10 text-teal-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                />
+              </svg>
+
+              <h2 className="mt-4 text-xl font-bold text-white">
+                Road conditions
+              </h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                The road conditions up to Cypress can vary depending on the
+                weather, check out the current road conditions here.
+              </p>
+            </a>
+
+            <a
+              className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:border-teal-500/10 hover:shadow-teal-500/10"
+              href="/services/digital-campaigns"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-10 h-10 text-teal-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                />
+              </svg>
+
+              <h2 className="mt-4 text-xl font-bold text-white">
+                Weather conditions
+              </h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                Please check the weather here before coming up and dress
+                appropriately for the conditions.
+              </p>
+            </a>
+
+            <a
+              className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:border-teal-500/10 hover:shadow-teal-500/10"
+              href="/services/digital-campaigns"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-10 h-10 text-teal-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                />
+              </svg>
+
+              <h2 className="mt-4 text-xl font-bold text-white">Food</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                There are a few places to eat up Cypress but I would recommend
+                bringing a snack and some water for the duration of the session.
+              </p>
+            </a>
+
+            <a
+              className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:border-teal-500/10 hover:shadow-teal-500/10"
+              href="/services/digital-campaigns"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-10 h-10 text-teal-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                />
+              </svg>
+
+              <h2 className="mt-4 text-xl font-bold text-white">Gear</h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                Safety is super important when skiing so make sure you're
+                bringing a helmet and gloves for the lesson as well as a warm
+                jacket and pants for the day.
+              </p>
+            </a>
+
+            <a
+              className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:border-teal-500/10 hover:shadow-teal-500/10"
+              href="/services/digital-campaigns"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-10 h-10 text-teal-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                />
+              </svg>
+
+              <h2 className="mt-4 text-xl font-bold text-white">
+                Digital campaigns
+              </h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                quo possimus adipisci distinctio alias voluptatum blanditiis
+                laudantium.
+              </p>
+            </a>
+
+            <a
+              className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:border-teal-500/10 hover:shadow-teal-500/10"
+              href="/services/digital-campaigns"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-10 h-10 text-teal-500"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                />
+              </svg>
+
+              <h2 className="mt-4 text-xl font-bold text-white">
+                Digital campaigns
+              </h2>
+
+              <p className="mt-1 text-sm text-gray-300">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                quo possimus adipisci distinctio alias voluptatum blanditiis
+                laudantium.
+              </p>
+            </a>
+          </div>
+
+          <div className="mt-12 text-center">
+            <a
+              href="#"
+              className="inline-flex items-center px-8 py-3 mt-8 text-white rounded bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 hover:bg-transparent focus:outline-none focus:ring "
+            >
+              <span className="text-sm font-medium"> Get Started </span>
+
+              <svg
+                className="w-5 h-5 ml-3"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Before;
