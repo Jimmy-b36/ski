cff0753d99ee025d9f52012776dbfba8148d4579
James Ball <jamie.j.ball@gmail.com>
2022-11-07 13:42:23 -0800

    CSS: add media widths

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      xs: '375px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {},
   },
   daisyui: {
